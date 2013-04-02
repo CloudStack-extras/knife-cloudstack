d99b523a4e98702bcfead550c2609f2f064d9efe
Frank Breedijk <fbreedijk@schubergphilis.com>
2013-04-02 20:08:59 +0200

    Template create, extract and register

--- lib/knife-cloudstack/connection.rb
+++ lib/knife-cloudstack/connection.rb
@@ -1,6 +1,7 @@
 #
 # Author:: Ryan Holmes (<rholmes@edmunds.com>)
 # Author:: KC Braunschweig (<kcbraunschweig@gmail.com>)
+# Author:: Frank Breedijk (<fbreedijk@schubergphilis.com>)
 # Copyright:: Copyright (c) 2011 Edmunds, Inc.
 # License:: Apache License, Version 2.0
 #
@@ -688,11 +689,10 @@ module CloudstackClient
 
       params_arr = []
       params.sort.each { |elem|
-        params_arr << elem[0].to_s + '=' + elem[1].to_s
+        params_arr << elem[0].to_s + '=' + CGI.escape(elem[1].to_s).gsub('+', '%20').gsub(' ','%20')
       }
       data = params_arr.join('&')
-      encoded_data = URI.encode(data.downcase).gsub('+', '%20').gsub(',', '%2c')
-      signature = OpenSSL::HMAC.digest('sha1', @secret_key, encoded_data)
+      signature = OpenSSL::HMAC.digest('sha1', @secret_key, data.downcase)
       signature = Base64.encode64(signature).chomp
       signature = CGI.escape(signature)
 
